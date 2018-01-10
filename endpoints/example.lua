d92a45e6ed5964f77564357a003a141a6f03ccf2
Cola Cheng <colacheng@exosite.com>
2018-01-10 22:56:16 +0800

    Add webservice config.

--- endpoints/example.lua
+++ endpoints/example.lua
@@ -1,5 +1,5 @@
 --#ENDPOINT POST /api/user
-print("Creating a new user")
+return "Creating a new user"
 
 --#ENDPOINT GET /api/user/{userId}
-print("Fetch a given user" .. request.parameters.userId)
+return "Fetch a given user" .. request.parameters.userId
